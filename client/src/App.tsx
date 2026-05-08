2c2e6e77908ba6ef09bca4ccc6c72df8f96ce510
micasgsl <159489395+micasgsl@users.noreply.github.com>
2026-05-08 11:12:12 -0400

    Fix GitHub Pages route handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,8 @@ function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/acessa-paraguacu"} component={Home} />
+      <Route path={"/acessa-paraguacu/"} component={Home} />
       <Route path={"/404"} component={NotFound} />
       <Route component={NotFound} />
     </Switch>
